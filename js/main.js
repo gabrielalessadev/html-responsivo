0d569258b956e7bd4c5ca7b7fb24a75c3ccf5d49
Gabriel <gabrielaledev@gmail.com>
2023-05-07 03:28:30 -0300

    sim

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ $("#navbar a, .btn").on("click", function (event) {
 
         $("html, body").animate(
             {
-                scrollTop: $(hash).offset().top - 100
+                scrollTop: $(hash).offset().top - 80
             },
-            800
+            900
         );
     }
 });
 
 // Sticky menu background
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 150) {
+    if (window.scrollY > 130) {
         document.querySelector("#navbar").style.opacity = 0.9;
     } else {
         document.querySelector("#navbar").style.opacity = 1;
